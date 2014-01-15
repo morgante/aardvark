918c822c941a30c2bdfb9ebfffa79faacdba2e49
Morgante Pell <morgante.pell@morgante.net>
2014-01-15 14:58:59 +0400

    get simple version to work, with caches and regex

--- aardvark.py
+++ aardvark.py
@@ -4,7 +4,21 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from cStringIO import StringIO
 
+import re
+import os.path
+
+def write_text(path, text):
+    file = open(path, "w")
+    file.write(text)
+    file.close()
+
 def get_text(path):
+    txt_path = path + '.txt'
+
+    if (os.path.isfile(txt_path)):
+        return open(txt_path).read()
+
+    path = path + '.pdf'
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
     codec = 'utf-8'
@@ -22,9 +36,39 @@ def get_text(path):
     device.close()
     str = retstr.getvalue()
     retstr.close()
+
+    write_text(txt_path, str)
+
     return str
 
-def 
+def get_acronyms(text):
+    pattern = r'\b(?:[A-Z]){2,}\b'
 
-t = get_text('examples/nyuad.pdf')
-print t
+    acronyms = re.findall(pattern, text)
+
+    return acronyms
+
+def expand(acronym, text):
+    pattern = r'('
+
+    for i, s in enumerate(acronym):
+        pattern += '' + s + '[A-Za-z]+'
+        if (i < len(acronym) - 1):
+            pattern += ' '
+    pattern += ')'
+
+    matches = re.findall(pattern, text)
+    expansions = dict.fromkeys(matches, 1)
+    
+    return expansions
+
+p = 'examples/lte'
+
+text = get_text(p)
+
+table = {}
+
+for acronym in get_acronyms(text):
+    table[acronym] = expand(acronym, text)
+
+print table
