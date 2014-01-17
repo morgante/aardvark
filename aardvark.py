54e7c8437ee9501efaa5c078907fc8f1ecd99fdc
Morgante Pell <morgante.pell@morgante.net>
2014-01-17 19:44:22 +0400

    finish cats

--- aardvark.py
+++ aardvark.py
@@ -54,21 +54,38 @@ def get_acronyms(text):
 
     return acronyms
 
+def get_cat_name(id):
+    cats = {
+        1: 'Most Common',
+        2: 'Technology',
+        4: 'Government & Military',
+        8: 'Science & Medicine',
+        64: 'Business',
+        16: 'Organizations',
+        32: 'Slang & jargon'
+    }
+
+    return cats[id]
+
 def get_expansions(acronym):
-    url = 'http://www.acronymfinder.com/~/search/af.aspx?string=exact&Acronym=' + acronym
+    url = 'http://acronyms.thefreedictionary.com/' + acronym
+    path = 'data/lte.html'
 
-    # r  = requests.get(url)
-    # data = r.text
+    doc = parse(path).getroot()
 
-    # soup = BeautifulSoup(data)
-    doc = parse(url).getroot()
+    defs = []
 
-    cells = doc.cssselect('table#ListResults tr:nth-child(n+3) td:nth-child(3)')
+    rows = doc.cssselect('table#AcrFinder tr:nth-child(n+1)')
 
-    defs = {}
+    # Fill in out definitions
+    for i, row in enumerate(rows):
+        category = row.get('cat')
+        cells = row.cssselect('td')
 
-    for cell in cells:
-        defs.append(cell.text_content())
+        if (len(cells) >= 2):
+            meaning = cells[1].text_content()
+            score = float(float(len(rows) - i) / float(len(rows)))
+            defs.append([meaning, category, score])
 
     return defs;
 
@@ -82,7 +99,16 @@ def expand(acronym, text):
     pattern += ')'
 
     matches = re.findall(pattern, text)
-    expansions = dict.fromkeys(matches, 1)
+    expansions = dict.fromkeys(matches, 10)
+
+    for expansion in get_expansions(acronym):
+        score = int(expansion[2] * 10)
+
+        if expansion[0] in expansions:
+            expansions[expansion[0]] += score
+        else:
+            expansions[expansion[0]] = score
+
 
     return expansions
 
@@ -92,8 +118,6 @@ text = get_text(p)
 
 acronyms = get_acronyms(text)
 table = {}
-
-print get_expansions(acronyms[0])
 
 for acronym in get_acronyms(text):
     table[acronym] = expand(acronym, text)
