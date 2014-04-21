983518cff3ff3dbd2d4d3bd563d6459ca75cc4de
Morgante Pell <morgante.pell@morgante.net>
2014-04-21 14:26:18 +0400

    something

--- Aardvark/test_examples.py
+++ Aardvark/test_examples.py
@@ -1,6 +1,26 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
+# content of test_module.py
+import pytest
 
-def test_answer():
-    assert func(3) == 5
+@pytest.fixture(scope="module")
+def modarg(request):
+	val = getattr(request.module, "lol", "merlinux.eu")
+	print "create it"
+	def fin():
+		print "finished it"
+	return val
+
+
+
+lol = 's'
+
+lol = 'pqr'
+
+def test_something(modarg):
+	assert 0, modarg
+
+# def test_0(otherarg):
+#     print "  test0", otherarg
+# def test_1(modarg):
+#     print "  test1", modarg
+# def test_2(otherarg, modarg):
+#     print "  test2", otherarg, modarg
