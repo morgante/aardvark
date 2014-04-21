27215e7cb74c3d5f3d9190b80fca93447b0df95e
Morgante Pell <morgante.pell@morgante.net>
2014-04-21 13:56:03 +0400

    start regression testing script

--- Aardvark/test.py
+++ Aardvark/test.py
@@ -1,15 +1,61 @@
-import vark
+import unittest
+import db
 
-p = 'examples/wef2'
+class TestResearch(unittest.TestCase):
 
-text = vark.get_text(p)
+	@classmethod
+	def setUpClass(cls):
+		cls.a = cls.something
 
-acronyms = vark.get_acronyms(text)
-table = {}
+	def test_value(self):
+		print 'hello'
+		self.assertEqual(self.a, 'b')
 
-print acronyms
+# def gen_test(a, b):
+#     def test(self):
+#         self.assertEqual(a, b)
+#     return test
 
-for acronym in acronyms:
-	table[acronym] = vark.expand(acronym, text)
+# Fetch test cases from database
+examples = db.find('research')
 
-print json.dumps(table)
+# for doc in examples:
+	# print doc
+
+# for t in l:
+# 	test_name = 'test_%s' % t[0]
+# 	test = test_generator(t[0], t[1])
+# 	setattr(TestResearch, test_name, test);
+
+def suite():
+	suite = unittest.TestSuite()
+	
+	test = TestResearch('test_value')
+	test.something ='s'
+	suite.addTest(test)
+
+	test2 = TestResearch('test_value')
+	test.something = 'l'
+	suite.addTest(test)
+
+	return suite
+
+if __name__ == '__main__':
+	suite = suite()
+	res = unittest.TextTestRunner(verbosity=2).run(suite)
+
+# import vark
+
+# p = 'examples/wef2'
+
+# text = vark.get_text(p)
+
+# acronyms = vark.get_acronyms(text)
+# table = {}
+
+# print acronyms
+
+# for acronym in acronyms:
+# 	table[acronym] = vark.expand(acronym, text)
+
+# print json.dumps(table)
